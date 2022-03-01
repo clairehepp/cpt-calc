56b0e5129157ed445742ed348dc07de653aad850
clairehepp <100723472+clairehepp@users.noreply.github.com>
2022-03-01 13:03:01 -0700

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,1 +1,11 @@
+from PySide6 import QtCore, QtGui
+from PySide6.QtWidgets import QMainWindow, QWidget, QPushButton, QLineEdit, QGridLayout, QHBoxLayout, QTextEdit
 
+class Calc(QMainWindow):
+  def __init__(self):
+    super().__init__()
+    
+    # Create base layout stuff
+    self.centralwidget = QWidget()
+    self.baselayout = QGridLayout()
+    self.centralwidget.setLayout(baselayout)
